0d7e923f37fd4c86b35a6ed28ed8d651017fbd93
Anastasiia Dankevych <150438964+ADankevych@users.noreply.github.com>
2025-03-31 22:34:01 +0300

    Update file_limit.cpp

--- file_limit.cpp
+++ file_limit.cpp
@@ -9,16 +9,16 @@ int main() {
     std::vector<std::ofstream> files;
 
     for (int i = 0; i < 100000; i++) {
-        std::string filename = "file_" + std::to_string(i+1) + ".txt";
+        std::string filename = "file_" + std::to_string(i + 1) + ".txt";
         files.emplace_back(filename);
 
         if (!files.back()) {
-            std::cerr << "Failed to open " << filename << " at iteration " << i+1 << "\n";
+            std::cerr << "Failed to open " << filename << " at iteration " << i + 1 << "\n";
             break;
         }
 
-        if ((i+1) % 1000 == 0) {
-            std::cout << "Opened " << i << " files\n";
+        if ((i + 1) % 1000 == 0) {
+            std::cout << "Opened " << i + 1 << " files\n";
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
     }
